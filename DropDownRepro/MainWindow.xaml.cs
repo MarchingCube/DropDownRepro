3e7cc3e5347d9cea5820a7e120dbf7de30e69264
Dariusz Komosinski <dariusz@gritworld.com>
2018-04-12 12:06:45 +0200

    !A Added small repro for DropDown click pass-through bug in Avalonia.

---
+++ DropDownRepro/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+
+namespace DropDownRepro
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+	        var statusPopup = this.FindControl<Popup>("StatusPopup");
+	        var statusText = this.FindControl<TextBlock>("StatusText");
+
+	        this.FindControl<Button>("Abort").Click += (sender, args) =>
+	        {
+		        statusPopup.IsOpen = true;
+		        statusText.Text = "Launch aborted!";
+	        };
+
+	        this.FindControl<Button>("Launch").Click += (sender, args) =>
+	        {
+		        statusPopup.IsOpen = true;
+				statusText.Text = "Launch successful!";
+	        };
+		}
+    }
+}
